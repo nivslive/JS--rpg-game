5e8fba99346556ddfc2562ec956042dbe51df744
Nivs <nivsoficial@gmail.com>
2023-06-11 17:11:38 -0300

    atts

---
+++ Sprite.js
@@ -0,0 +1,36 @@
+class Sprite {
+    constructor(config) {
+
+        //set up the image
+        this.image = new Image()
+        this.image.src = config.src
+
+        // configure animation and initial state
+        this.animations = config.animations || {
+            idleDown: [
+                [0, 0]
+            ],
+        }
+        this.currentAnimation = config.currentAnimation || 'idleDown';
+        this.currentAnimationFrame = 0;
+
+
+        //reference to GameObject
+        this.gameObject = config.gameObject
+    }
+
+
+    draw(ctx) {
+        const x = this.gameObject.x * 16 - 8;
+        const y = this.gameObject.y * 16 - 18;
+
+
+        ctx.drawImage(
+            this.image,
+            0,0,
+            32,32,
+            x,y,
+            32,32
+        );
+    }
+}
